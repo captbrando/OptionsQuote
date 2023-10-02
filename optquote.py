09afa26151485f9c9b0424339a3d63fb9f2a93eb
Branden R. Williams <brw@brandolabs.com>
2023-10-02 16:57:14 -0500

    Fixed to use the Yahoo Finance python package (yfinance) and addressed the deprecation of the cgi package.

--- optquote.py
+++ optquote.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
-from bs4 import BeautifulSoup
-import requests
-import cgi, cgitb
-import string
+import yfinance as yf
+import os
+from urllib.parse import parse_qsl
 
-# Get the specific contract we want passed in the GET request.
-args = cgi.FieldStorage()
-symbol = args.getvalue('c').strip()
+# Get the query string from the environment variables
+query_string = os.environ.get("QUERY_STRING", "")
 
-# Build our URL and get the HTML back and parse with BeautifulSoup
-url = 'https://finance.yahoo.com/quote/' + symbol + '?p=' + symbol
-r = requests.get(url)
-soup = BeautifulSoup(r.text,'lxml')
+# Parse the query string
+query_params = dict(parse_qsl(query_string))
 
-# This is easily found with any number of dev tools, but its the "name" 
-# of the HTML tag that has the actual value inside. There is only one
-# tag with this name (HUZZUH!) so we can just use the find() method.
-current_price = soup.find('span', {'class':'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'})
+# Get the args from the URL string.
+symbol = query_params.get('s').split(":")[1]
+contract = query_params.get('c')
+expiration = query_params.get('d')
+opt_type = query_params.get('o')
+
+# Get the last price on the contract we want.
+stock = yf.Ticker(symbol)
+option = stock.option_chain(date=expiration)
+
+match opt_type:
+        case "Call":
+                lastPrice = option.calls[option.calls["contractSymbol"] == contract].lastPrice
+
+        case "Put":
+                lastPrice = option.calls[option.puts["contractSymbol"] == contract].lastPrice
 
 # OK, here's lazybones Brando coming out. I could make this a proper API 
 # to return a JSON object or something, but given the integration with 
 # Google Sheets, this was the easiest way to reliably get the value out.
 print("Content-type:text/html\r\n\r\n")
-print(current_price.text)
+print(lastPrice.values[0])
+
